ab7e6f51fb767840e8225d257b9deac2bb659339
Harout360 <gamemasterhg@sbcglobal.net>
2015-11-25 20:20:16 -0800

    Project upload

---
+++ src/kiloboltgame/Animation.java
@@ -0,0 +1,12 @@
+package kiloboltgame;
+
+import java.util.ArrayList;
+
+public class Animation {
+
+	private ArrayList frames;
+	private int currentFrame;
+	private long animTime; //long takes up more memory than int but can hold more accurate numbers.
+	private long totalDuration;
+
+}
